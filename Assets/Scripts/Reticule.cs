f241ccfd0341b0a4ccfe66eb958212bc2fe22af5
Ahmed Goni <ai.elgoni@gmail.com>
2019-11-24 16:42:38 +0100

    Added reticule script

---
+++ Assets/Scripts/Reticule.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Reticule : MonoBehaviour
+{
+    private void Start()
+    {
+        Cursor.visible = false;
+    }
+
+    private void Update()
+    {
+        Vector3 mousePos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, -this.transform.position.z);
+        this.transform.position = Camera.main.ScreenToWorldPoint(mousePos);
+    }
+}
